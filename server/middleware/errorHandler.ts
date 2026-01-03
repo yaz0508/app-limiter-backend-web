9d23e293eac39709de5af1f5f547c13bf896bc6d
yaz <ptdestroyer12@gmail.com>
2026-01-03 18:50:52 +0800

    Fix TypeScript error in errorHandler - setHeader only accepts 2 arguments

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -119,7 +119,7 @@ function setCorsHeaders(req: Request, res: Response) {
         if (isAllowed) {
             res.setHeader("Access-Control-Allow-Origin", origin);
             res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-            res.setHeader("Access-Control-Allow-Headers", "Content-Type", "Authorization");
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         }
     }
 }
